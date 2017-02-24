4908e8934aed4324a5222ed70ff605c0abfb457b
Will <wchen8878@gmail.com>
2017-02-24 15:54:39 -0500

    completed the Login function

--- Water_Report/app/src/main/java/com/example/weichen/water_report/controllers/LoginActivity.java
+++ Water_Report/app/src/main/java/com/example/weichen/water_report/controllers/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.example.weichen.water_report.controllers;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.weichen.water_report.R;
 import com.example.weichen.water_report.model.User_Infor;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,14 +28,17 @@ public class LoginActivity extends AppCompatActivity {
 
     private ImageButton _goBack;
 
-    private User_Infor user_infor;
+    private FirebaseAuth loginAuth;
 
-    private boolean result = true;
+    private FirebaseAuth.AuthStateListener loginAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginAuth = FirebaseAuth.getInstance();
 
         userName = (EditText) findViewById(R.id.user_name_input);
         if (userName.getText().toString().length() == 0) {
@@ -41,29 +51,47 @@ public class LoginActivity extends AppCompatActivity {
         _login = (Button) findViewById(R.id.login_button);
         _goBack = (ImageButton) findViewById(R.id.go_back_button);
 
-
-
-
+        loginAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (userName != null) {
+                    // User is signed in
+                } else {
+                    // User is signed out
+                }
+            }
+        };
     }
 
     /**
      * Us to test the password and username
      */
     public void login(View view ) {
+
+        String _username = userName.getText().toString();
+        String _password = passWord.getText().toString();
+
         if (passWord == null || userName == null) {
             userName.setError("User Name can't be empty!");
             passWord.setError("Password can't be empty!");
         } else {
-//            result = user_infor.checkLogin(userName.getText().toString(), passWord.getText().toString());
+            loginAuth.signInWithEmailAndPassword(_username,_password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+                                Intent intents = new Intent(LoginActivity.this, WelcomActivity.class);
+                                startActivity(intents);
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Email and Password are not match!", Toast.LENGTH_SHORT).show();
+                            }
 
-            if (userName.getText().toString().equals("user")|| passWord.getText().toString().equals("pass")) {
-
-                        Intent intents = new Intent(LoginActivity.this, WelcomActivity.class);
-                        startActivity(intents);
-            } else {
-                passWord.setError("User or Password may not be correct!");
-            }
+                        }
+                    });
         }
+
+
     }
 
 
@@ -77,6 +105,20 @@ public class LoginActivity extends AppCompatActivity {
                 userName = null;
                 startActivity(intents);
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        loginAuth.addAuthStateListener(loginAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (loginAuthListener != null) {
+            loginAuth.removeAuthStateListener(loginAuthListener);
+        }
     }
 
 }

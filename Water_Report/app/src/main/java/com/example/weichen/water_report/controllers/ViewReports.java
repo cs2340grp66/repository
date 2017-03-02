f08d3054351678ea2b611c766747d294d21f30ce
dylanpatel95 <dylanpatel95@gmail.com>
2017-03-02 13:08:44 -0500

    dylan

---
+++ Water_Report/app/src/main/java/com/example/weichen/water_report/controllers/ViewReports.java
@@ -0,0 +1,13 @@
+package com.example.weichen.water_report.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewReports extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_reports);
+    }
+}

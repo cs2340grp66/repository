514dce373aa8b4932c24a78feb9d0093481a6046
Will <wchen8878@gmail.com>
2017-03-14 13:02:11 -0400

    basic setup for google map setup on M8

---
+++ Water_Report/app/src/main/java/com/example/weichen/water_report/controllers/Manager_Welcome_Activity.java
@@ -0,0 +1,15 @@
+package com.example.weichen.water_report.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.weichen.water_report.R;
+
+public class Manager_Welcome_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager__welcome_);
+    }
+}
